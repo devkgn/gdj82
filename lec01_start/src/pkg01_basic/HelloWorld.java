d7263b9e34e6a51f44741ceb4716ffa8bf5b9413
devkgn <devkgn13@gmail.com>
2024-04-18 08:42:54 +0900

    [김가남] 자바 개요 업로드

---
+++ lec01_start/src/pkg01_basic/HelloWorld.java
@@ -0,0 +1,20 @@
+package pkg01_basic;
+
+public class HelloWorld {
+	// 메인 메소드의 선언부
+	public static void main(String[] args) {
+		// 실행될 코드
+		/* 블록 주석 연습하기 */
+		System.out.print("Hello World~");
+		System.out.print(123);
+		// 줄바꾸기 있는 표준출력
+		System.out.println("hello world");
+		System.out.println(123);
+		// 이스케이프 문자
+		// "안녕하세요~"
+		System.out.println("안녕하세요");
+		System.out.println("\"안녕하세요\"");
+		// hello 와 world 사이 줄바꾸기
+		System.out.println("hello\nworld");
+	}
+}
