345a522702f6b8c42c8780629e9dfd0eb2e237c6
devkgn <devkgn13@gmail.com>
2024-04-18 15:37:51 +0900

    [김가남] 형변환

--- lec02_variable/src/pkg03_casting/MainClass.java
+++ lec02_variable/src/pkg03_casting/MainClass.java
@@ -3,13 +3,50 @@ package pkg03_casting;
 public class MainClass {
 
 	public static void main(String[] args) {
-		// 값의 범위가 작은 데이터가
+		// 업캐스팅 == 자동형변환
+		// 1. 값의 범위가 작은 데이터가
 		// 값의 범위가 큰 변수에 할당될 경우
 		int a = 23;
 		double b = a;
 		System.out.println(a);
 		System.out.println(b);
-
+		// 2. 값의 범위가 다른 2개의 값이 산술 연산
+		int c = 3;
+		double d = 20.5;
+		// 3 -> 3.0 -> 3.0 + 20.5
+		System.out.println(c+d);
+		// 3. char 타입 변수
+		char f = '2';
+		// 50 + '2' = 100
+		System.out.println(50+f);
+		// char 타입 업캐스팅 활용
+		// '2' 아스키값으로 형변환하면 50
+		// '2' - 48 = 2
+		// '2' - '0' = 2
+		System.out.println(f-'0');
+		// (1)사용자가 입력한 문자 '5'를 
+		// 숫자 5로 가장 빠르게 변환하는 방법은?
+		System.out.println('5'-'0');
+		
+		// 다운캐스팅
+		double d_num = 3.14;
+		int i_num = 4;
+		System.out.println(d_num+i_num);
+		System.out.println((int)d_num);
+		System.out.println((int)d_num+i_num);
+		// char의 다운캐스팅
+		char apple = 'A';
+		int numApple = (int)apple;
+		System.out.println(apple);
+		System.out.println(numApple);
+		System.out.println((int)'a');
+		
+		// (2) 소문자 -> 대문자로 변환
+		// 아스키 : 소문자, 대문자 사이의 차이가 32
+		char char1 ='a';
+		char char2 = (char)(char1-32);
+		System.out.println(char1-32);
+		System.out.println(char2);
 	}
 
 }
---
+++ lec02_variable/src/pkg03_casting/Practice01.java
@@ -0,0 +1,20 @@
+package pkg03_casting;
+
+public class Practice01 {
+
+	public static void main(String[] args) {
+		/*
+		 * 아래의 변수들을 선언, 초기값을 할당하고 
+		 * 출력하는 프로그램을 작성하세요. 
+		 * 1. 변수 num1에는 정수형 데이터 10을 저장합니다. 
+		 * 2. 변수 num2에는 실수형 데이터 5.7을 저장합니다. 
+		 * 3. num1과num2를 더한 결과를 출력합니다.
+		 * 
+		 * 출력 예시)
+		 * 정수 : 15
+		 * 실수 : 15.7
+		 */
+
+	}
+
+}
