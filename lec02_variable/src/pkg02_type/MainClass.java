89c464c70bf0e64ac073e842a69bf7e2625400ca
devkgn <devkgn13@gmail.com>
2024-04-18 12:49:54 +0900

    [김가남] 기본형과 참조형 비교

--- lec02_variable/src/pkg02_type/MainClass.java
+++ lec02_variable/src/pkg02_type/MainClass.java
@@ -17,6 +17,37 @@ public class MainClass {
 		// 문자 하나, 홑따옴표
 		char grade = '김';
 		// char gender = 'Female';
+		// 논리형 변수
+		boolean isStudent = true;
+		System.out.println(isStudent);
+		
+		//String(문자열)
+		String str1 = new String("감사");
+		// 1. 표준출력
+		System.out.println(str1);
+		// 2. 재할당
+		str1 = new String("합니다.");
+		System.out.println(str1);
+		// 3. 다른 변수 값
+		String str2 = str1;
+		System.out.println(str2);
+		// 기본형처럼 초기화
+		String str3 = "감사합니다.";
+		System.out.println(str3);
+		System.out.println(1==1);
+		// 기본형 비교
+		int num1 = 2;
+		int num2 = 2;
+		System.out.println(num1==num2);
+		// 참조형 비교
+		String text1 = new String("apple");
+		String text2 = new String("apple");
+		System.out.println(text1 == text2);
+		// +부호
+		// 부호 사이에 문자열이 하나 이상
+		System.out.println(1+2);
+		System.out.println(1+" : "+2);
+		
 	}
 
 }
---
+++ lec02_variable/src/pkg02_type/Practice01.java
@@ -0,0 +1,48 @@
+package pkg02_type;
+
+public class Practice01 {
+
+	public static void main(String[] args) {
+		/*
+		 * 로맨틱한 개발자 김철수는 여자친구에게 
+		 * 사랑이 넘치는 코드를 쓰려고 합니다.
+		 * 아래 지시 사항을 보고 적합한 변수를 선언해주세요.
+		 * 초기화한 후 출력까지 해보기!!
+		 * 1. 변수명이 love인 논리형 변수 
+		 * 2. 너에게 별을 따주고 싶은 마음을 나타내는 char 변수 star 
+		 * 3. 너를 향해 가는 길의 길이 byte 변수 street 
+		 * 4. 너를 위해 준비한 선물의 개수 short 변수 gift 
+		 * 5. 우리가 사랑한 날 int 변수 day 
+		 * 6.우리 앞에 펼쳐진 수많은 날들 long 변수 future 
+		 * 7. 내 마음속 너와 나의 거리 float 변수 heart 
+		 * 8. 너를 사랑하지 않는 마음의 크기 double 변수 hate
+		 */
+		boolean love;
+		love = false;
+		System.out.println("love 출력 : "+love);
+		char star;
+		star = 'A';
+		System.out.println("star 출력 : "+star);
+		byte street;
+		street = 1;
+		System.out.println("street 출력 : "+street);
+		short gift;
+		gift = 23;
+		System.out.println("gift 출력 : "+gift);
+		int day;
+		day = 1000;
+		System.out.println("day 출력 : "+day);
+		long future;
+		future = 222;
+		System.out.println("future 출력 : "+future);
+		float heart;
+		heart = 2.3f;
+		System.out.println(heart);
+		double hate;
+		hate = 0.0000001;
+		System.out.println(hate);
+		
+
+	}
+
+}
