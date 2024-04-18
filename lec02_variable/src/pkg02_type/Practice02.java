95d36074945419cd2ce6628dad2d1ac538a08faf
devkgn <devkgn13@gmail.com>
2024-04-18 14:29:23 +0900

    [김가남] charAt 연습하기

--- lec02_variable/src/pkg02_type/Practice02.java
+++ lec02_variable/src/pkg02_type/Practice02.java
@@ -8,6 +8,14 @@ public class Practice02 {
 		 * 풀네임에서 이름만 추출하는 프로그램을 작성해주세요.
 		 * 황보솔 학생은 성이 “황보”이고, 이름이 “솔”입니다.
 		 */
+		String name1 = "황보솔";
+		String name2 = new String("황보솔");
+		// 이름만 가져오기
+		char first = name1.charAt(0);
+		char second = name1.charAt(1);
+		char third = name1.charAt(2);
+		System.out.println("제 이름은 \""+third+"\"이라구요!!");
+		System.out.println("성이 "+first+second+"입니다.");
 	}
 
 }
