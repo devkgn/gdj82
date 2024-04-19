f3c035b3f19864cab611e05eda8c2b6d9596a99b
devkgn <devkgn13@gmail.com>
2024-04-19 10:07:20 +0900

    [김가남] prinf 코드

--- lec01_start/src/practice/Introduce.java
+++ lec01_start/src/practice/Introduce.java
@@ -5,5 +5,10 @@ public class Introduce {
 		System.out.print("안녕하세요~\n");
 		System.out.print("저는 \"김철수\"입니다.\n");
 		System.out.print("나이는 20살이고, mbti는 istp입니다.\n");
+		
+		char c1 = 'a'; 
+		char c2 = 'd';
+		// 두개의 문자 크기 비교
+		System.out.println(c1 < c2);
 	}
 }
---
+++ lec02_variable/src/pkg02_type/Homework01.java
@@ -0,0 +1,20 @@
+package pkg02_type;
+// 보건교사 김철수
+public class Homework01 {
+	
+	public static void main(String[] args) {
+		
+		// System.out.printf("표현식",출력값);
+		System.out.printf("%d",3);
+		System.out.printf("%c",'A');
+		// %5d : 왼쪽 5칸을 확보, 오른쪽 정렬
+		System.out.printf("%5d", 1);
+		// %-5d : 오른쪽 5칸 확보, 왼쪽 정렬
+		System.out.printf("%-5d",2);
+		// %.2f : 소수점 아래 2자리까지만 표시
+		System.out.printf("%.2f",3.141592653589793);
+		
+	}
+	
+
+}
