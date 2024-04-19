b55c9c37ce5bb4088dce9a0a99b4dcddbdfb6aba
devkgn <devkgn13@gmail.com>
2024-04-19 10:50:32 +0900

    [김가남] 산술연산자, 형변환

--- lec03_operator/src/pkg01_arithmetic/MainClass.java
+++ lec03_operator/src/pkg01_arithmetic/MainClass.java
@@ -20,5 +20,18 @@ public class MainClass {
 		// 나누기 나머지
 		int remain = su1 % su2;
 		System.out.println("% : "+remain);
+		// 산술 연산과 형변환
+		// 점수는 정수형 , 평균은 실수
+		int math = 33;
+		int science = 82;
+		int sum = math + science; // 115
+		// 1. 평균(정수)
+		int avg1 = sum / 2;
+		System.out.println(avg1);
+		// 2. 평균(실수)
+		double avg2 = sum / 2.0;
+		System.out.println(avg2);
+		
+		
 	}
 }
--- lec03_operator/src/pkg01_arithmetic/Practice01.java
+++ lec03_operator/src/pkg01_arithmetic/Practice01.java
@@ -1,5 +1,7 @@
 package pkg01_arithmetic;
 
+import java.util.Scanner;
+
 public class Practice01 {
 	/*
 	 * 철수는 오늘 학교에서 산술 연산자 +, -, *, /, %에 대해서 배웠습니다.
@@ -13,4 +15,30 @@ public class Practice01 {
 	   나누기 : 6
 	   나머지 : 2
 	 */
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		// 정수 입력 받기
+		System.out.print("첫번째 숫자 : ");
+		int su1 = sc.nextInt();
+		System.out.print("두번째 숫자 : ");
+		int su2 = sc.nextInt();
+		// 더하기
+		int multiple = su1 + su2;
+		System.out.println("덧셈 : "+multiple);
+		// 빼기
+		int diff = su1 - su2;
+		System.out.println("뺄셈 : "+diff);
+		// 곱하기
+		int product = su1 * su2;
+		System.out.println("곱셈 : "+product);
+		// 나누기 몫
+		int quotient = su1 / su2;
+		System.out.println("나누기 : "+quotient);
+		// 나누기 나머지
+		int remain = su1 % su2;
+		System.out.println("나머지 : "+remain);
+		
+		sc.close();
+	}
 }
