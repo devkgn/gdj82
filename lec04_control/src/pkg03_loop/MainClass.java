2af826c08f9b2f86cb4143c1810e514e2ce82dc3
devkgn <devkgn13@gmail.com>
2024-04-22 15:17:10 +0900

    [김가남] for문

--- lec04_control/src/pkg03_loop/MainClass.java
+++ lec04_control/src/pkg03_loop/MainClass.java
@@ -18,5 +18,33 @@ public class MainClass {
 			// 6. 조건식 : i <= 2 -> false -> for문 종료
 			System.out.println(i);
 		}
+		
+		System.out.println("Hello, World");
+		System.out.println("Hello, World");
+		System.out.println("Hello, World");
+		System.out.println("Hello, World");
+		System.out.println("Hello, World");
+		System.out.println("Hello, World");
+		System.out.println("Hello, World");
+		System.out.println("Hello, World");
+		System.out.println("Hello, World");
+		System.out.println("Hello, World");
+
+		for(int i = 0 ; i < 10 ; i++) {
+			System.out.println(i+" :Hello, World");
+		}
+		
+		// 초기식 변수 
+		// 1. 블록 안에서만 사용 가능
+		// 2. i만 쓸 수 있는 것 아님!!!
+		// 3. 실수 변수를 사용 하지 않음
+		for(int a = 1 ; a <= 2 ; a++) {
+			System.out.println("a : "+a);
+		}
+		
+		
+		
+		
+		
 	}
 }
