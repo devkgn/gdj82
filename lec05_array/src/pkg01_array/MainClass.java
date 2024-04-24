ab7e31b48efa422bff5a5427fc12de75d46df637
devkgn <devkgn13@gmail.com>
2024-04-24 15:31:18 +0900

    [김가남] 배열 선언, 생성, 초기화

--- lec05_array/src/pkg01_array/MainClass.java
+++ lec05_array/src/pkg01_array/MainClass.java
@@ -36,7 +36,30 @@ public class MainClass {
 		System.out.println(str.length());
 		
 		// 배열의 초기화
-		
+		// 1. 인덱스를 이용 : 보통 규칙이 없는 경우
+		int[] age1 = new int[3];
+		System.out.println("초기화 전 : "+age1[1]);
+		age1[0] = 30;
+		age1[1] = 20;
+		age1[2] = 70;
+		System.out.println("초기화 후 : "+age1[1]);
+		age1[1] = 50;
+		System.out.println("재할당 : "+age1[1]);
+		String[] fruits = new String[3];
+		// 2. 반복문을 이용 : 보통 규칙이 있는 경우
+		int[] age2 = new int[4];
+		// 10, 11, 12, 13
+		// 0, 1, 2, 3
+		for(int i = 0 ; i < age2.length ; i++) {
+			age2[i] = i+10;
+		}
+		System.out.println(age2[3]);
+		// 3. 선언과 동시에 초기화
+		int[] age3 = new int[] {1,2,3,4};
+		int[] age4 = {5,6,7};
+		// 주의!!
+//		int[] age5;
+//		age5 = {9,10,11};
 		
 		
 	}
---
+++ lec05_array/src/pkg01_array/Parctice01.java
@@ -0,0 +1,10 @@
+package pkg01_array;
+
+public class Parctice01 {
+	public static void main(String[] args) {
+		// 철수의 친구 5명 이름을 담고 있는 배열 friends
+		// 선언, 생성한 다음 친구들의 이름 초기화
+		// 박지영, 김태우, 이지훈, 최유진, 정승호
+		// 크기 5짜리 문자열형 배열 구성
+	}
+}
