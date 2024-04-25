68763cea730dea9a5dfcf37394d72f27d7e0b21a
devkgn <devkgn13@gmail.com>
2024-04-25 16:12:12 +0900

    [김가남] 2차원 배열 연습문제

--- lec06_dimensional/src/pkg01_dimensional/Practice01.java
+++ lec06_dimensional/src/pkg01_dimensional/Practice01.java
@@ -7,5 +7,17 @@ public class Practice01 {
 		// 출력)
 		// (0,0)(0,1)(0,2)
 		// (1,0)(1,1)(1,2)
+		String[][] arr = new String[2][3];
+		for(int i = 0 ; i < arr.length ; i++) {
+			for(int j = 0 ; j < arr[i].length ; j++) {
+				arr[i][j] = "("+i+","+j+")";
+			}
+		}
+		for(int i = 0 ; i < arr.length ; i++) {
+			for(int j = 0 ; j < arr[i].length ; j++) {
+				System.out.print(arr[i][j]);
+			}
+			System.out.println();
+		}
 	}
 }
