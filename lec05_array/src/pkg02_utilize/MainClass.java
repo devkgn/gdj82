2b4f4e98872c969d1a181e540c014874fb22bb3b
devkgn <devkgn13@gmail.com>
2024-04-25 12:35:10 +0900

    [김가남] 배열 정렬하기

--- lec05_array/src/pkg02_utilize/MainClass.java
+++ lec05_array/src/pkg02_utilize/MainClass.java
@@ -33,5 +33,69 @@ public class MainClass {
 		}
 		System.out.println("최솟값 : "+min);
 		
+		// 오름차순
+		int[] data = {8,4,1,3};
+		// 1. 8(0)과 4(1) 비교
+		// 2. 4,8로 순서 변경
+		// 3. 4,8,1,3 -> 4(0)와 1(2)을 비교
+		// 4. 1,8,4,3 -> 1(0)과 3(3)을 비교
+		// 5. 8(1)과 4(2) 비교 -> 1,4,8,3
+		// 6. 4(1)와 3(3) 비교 -> 1,3,8,4
+		// 7. 8(2)과 4(3)비교 -> 1,3,4,8 
+		
+		// 0과 1
+		// 0과 2
+		// 0과 3
+		// 1과 2
+		// 1과 3
+		// 2과 3
+		
+		for(int i = 0 ; i < data.length -1 ; i++) {
+			for(int j = i + 1 ; j < data.length ; j++) {
+				// System.out.println(i+"과 "+j);
+				// 조건 성립 여부에 따라 위치 변경
+				if(data[i] > data[j]) {
+					int temp = data[i];
+					data[i] = data[j];
+					data[j] = temp;
+				}
+				
+			}
+		}
+		for(int i = 0 ; i < data.length ; i++) {
+			System.out.println(i+" : "+data[i]);
+		}
+		
+		
+		// 내림차순
+		int[] nums = {8,4,1,3};
+		
+		// 1. 8(0)과 4(1) 비교 -> 8,4,1,3
+		// 2. 8(0)과 1(2) 비교 -> 8,4,1,3
+		// 3. 8(0)과 3(3) 비교 -> 8,4,1,3
+		// 4. 4(1)와 1(2) 비교 -> 8,4,1,3
+		// 5. 4(1)와 3(3) 비교 -> 8,4,1,3
+		// 6. 1(2)와 3(3) 비교 -> 8,4,3,1
+		
+		for(int i = 0 ; i < nums.length -1 ; i++) {
+			for(int j = i +1 ; j < nums.length ; j++) {
+				if(nums[i] < nums[j]) {
+					int temp = nums[i];
+					nums[i] = nums[j];
+					nums[j] = temp;
+				}
+			}
+		}
+		
+		for(int i = 0 ; i < nums.length ; i++) {
+			System.out.println(i+" : "+nums[i]);
+		}
+		
+		
+		
+		
+		
+		
+		
 	}
 }
--- lec05_array/src/pkg02_utilize/Practice02.java
+++ lec05_array/src/pkg02_utilize/Practice02.java
@@ -1,5 +1,20 @@
 package pkg02_utilize;
 
 public class Practice02 {
-
+	public static void main(String[] args) {
+		int[] ages = {20,18,22,19,21};
+		// 최대값
+		int maxAge = ages[0];
+		int minAge = ages[0];
+		for(int i = 0 ; i < ages.length ; i++) {
+			if(ages[i] > maxAge) {
+				maxAge = ages[i];
+			}
+			if(ages[i] < minAge) {
+				minAge = ages[i];
+			}
+		}
+		System.out.println("가장 나이가 많은 학생 : "+maxAge);
+		System.out.println("가장 어린 학생 : "+minAge);
+	}
 }
