dce784a130ec42d0afbd969f136b2cf054be4ba7
devkgn <devkgn13@gmail.com>
2024-04-26 11:16:11 +0900

    [김가남] import문

--- lec07_oop/src/pkg01_class/controller/Run.java
+++ lec07_oop/src/pkg01_class/controller/Run.java
@@ -1,9 +1,18 @@
 package pkg01_class.controller;
 
+// import java.util.Date;
+// import java.util.Scanner;
+// * : 와일드 카드
+ import java.util.*;
+// import java.*;
+
 import pkg01_class.model.vo.Car;
 
 public class Run {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Date d = new Date();
+		
 		// 1. 참조 변수 선언
 		Car c1;
 		// 2. 객체 생성
@@ -15,6 +24,5 @@ public class Run {
 		// 동시 진행 가능
 		Car c2 = new Car();
 		String str2 = new String();
-		
 	}
 }
