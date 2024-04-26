9bcdac069d0bd4658bb6f70ef6910ea8833ff60f
devkgn <devkgn13@gmail.com>
2024-04-26 12:50:02 +0900

    [김가남] 필드의 접근 제한자, 예약어

---
+++ lec07_oop/src/pkg02_field/controller/Run.java
@@ -0,0 +1,29 @@
+package pkg02_field.controller;
+
+import pkg02_field.model.vo.Car;
+
+public class Run {
+	public static void main(String[] args) {
+		// 인스턴스화
+		Car c1 = new Car();
+		// 필드에 값 대입
+		// 변수명 = 값;
+		// c1 인스턴스의 이름을 소방차로 대입
+		// c1.name = "소방차";
+		// 필드에 값 호출
+		// 변수명
+		System.out.println(c1.name);
+		
+		// 필드 예약어 static
+		Car c2 = new Car();
+		Car c3 = new Car();
+		
+		c2.brand = "Hyundai";
+		System.out.println("c1 : "+c1.brand);
+		System.out.println("c2 : "+c2.brand);
+		System.out.println("c3 : "+c3.brand);
+		
+		
+		
+	}
+}
--- lec07_oop/src/pkg02_field/model/vo/Car.java
+++ lec07_oop/src/pkg02_field/model/vo/Car.java
@@ -2,4 +2,15 @@ package pkg02_field.model.vo;
 
 public class Car {
 	// [접근제한자] [예약어] 자료형 변수명 [= 초기값];
+	// 차 이름 -> 전체
+	// 인스턴스 변수 -> 클래스 변수
+	public static final String name = "avante";
+	// 브랜드 -> 전체
+	public static String brand;
+	// 색깔 -> 다른 패키지X
+	protected String color; 
+	// 제조년도 -> Car(현재) 클래스 안에서만 O
+	private int year;
+	// 가격 -> 같은 패키지 까지 O
+	int price;
 }
