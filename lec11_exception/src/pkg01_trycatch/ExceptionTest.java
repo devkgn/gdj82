77c033dd17f48249a222b7a9bced7344cb7b0dbd
devkgn <devkgn13@gmail.com>
2024-05-03 14:16:47 +0900

    [김가남] 다중 catch

---
+++ lec11_exception/src/pkg01_trycatch/ExceptionTest.java
@@ -0,0 +1,19 @@
+package pkg01_trycatch;
+
+public class ExceptionTest {
+	public static void main(String[] args) {
+		// 다중 catch
+		// 하위 클래스가 상단에 와야함
+		String[] arr = new String[3];
+		try {
+			System.out.println(arr[0].length());
+			System.out.println(arr[3]);
+		} catch(NullPointerException e) {
+			System.out.println("1. null은 길이를 알 수 없어요.");
+		} catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("2. 인덱스는 배열의 길이를 벗어날 수 없어요.");
+		} catch(Exception e) {
+			System.out.println("뭐든, 에가 생겼나봐요.");
+		}
+	}
+}
