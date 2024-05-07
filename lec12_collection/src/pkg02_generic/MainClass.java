4393a263984f2004c8852f7cafe98295b9162f2b
devkgn <devkgn13@gmail.com>
2024-05-07 11:17:22 +0900

    [김가남] 제네릭 사용하기

--- lec12_collection/src/pkg02_generic/MainClass.java
+++ lec12_collection/src/pkg02_generic/MainClass.java
@@ -7,5 +7,10 @@ public class MainClass {
 			= new Container<Integer>();
 		Container<String> c2 
 			= new Container<String>();
+		// 2. 제네릭 메소드
+		c1.setItem(50);
+		System.out.println(c1.getItem());
+		c2.setItem("안녕하세요.");
+		System.out.println(c2.getItem());
 	}
 }
