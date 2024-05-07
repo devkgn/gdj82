b732c89c6b8042bf276f0343c4ab8cb4a86daa10
devkgn <devkgn13@gmail.com>
2024-05-07 09:54:14 +0900

    [김가남]Wrapper 클래스

---
+++ lec12_collection/src/pkg01_wrapper/MainClass.java
@@ -0,0 +1,15 @@
+package pkg01_wrapper;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// Boxing : 기본형 -> 참조형
+		Integer iNum = 100;
+		// UnBoxing : 참조형 -> 기본형
+		int num = iNum;
+		boolean b = Boolean.parseBoolean("true");
+		
+		
+	}
+
+}
