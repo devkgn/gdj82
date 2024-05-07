499b06ce887c87e7cf76d8f488c930734e200d9b
devkgn <devkgn13@gmail.com>
2024-05-07 15:49:24 +0900

    [김가남] ArrayList 연습문제

---
+++ lec12_collection/src/pkg03_list/practice/Product.java
@@ -0,0 +1,25 @@
+package pkg03_list.practice;
+
+public class Product {
+	private String name;
+	private int price;
+	
+	public Product() {}
+	public Product(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	
+	@Override
+	public String toString() {
+		return name+"은(는) "+price+"원이에요.";
+	}
+}
---
+++ lec12_collection/src/pkg03_list/practice/Run.java
@@ -0,0 +1,39 @@
+package pkg03_list.practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Run {
+	public static void main(String[] args) {
+//		Product p1 = new Product("수박",10000);
+//		System.out.println(p1);
+//		
+//		Object o = new Object();
+//		System.out.println(o);
+		
+		List<Product> products = new ArrayList<Product>();
+		Product p1 = new Product("사과",1000);
+		products.add(p1);
+		products.add(new Product("바나나",2000));
+		products.add(new Product("딸기",3000));
+		
+		System.out.println("===== for each로 조회 =====");
+		for(Product p :products) {
+			System.out.println(p);
+		}
+		
+		products.remove(1);
+		products.set(0, new Product("오렌지",1500));
+		
+		System.out.println("===== 기본 for문 조회 =====");
+		for(int i = 0 ; i < products.size() ;i++) {
+			System.out.println(i+" : "+products.get(i));
+		}
+		
+		// 요소 검색
+		boolean isObj = products.contains(new Product("오렌지",1500));
+		System.out.println("오렌지 있어요? "+isObj);
+		
+		
+	}
+}
