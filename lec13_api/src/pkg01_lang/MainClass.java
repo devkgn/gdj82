f75f7f8c49cc8a3aff4e3a680dbe2d07ac556475
devkgn <devkgn13@gmail.com>
2024-05-13 09:50:03 +0900

    [김가남] String과 불변 객체

--- lec13_api/src/pkg01_lang/MainClass.java
+++ lec13_api/src/pkg01_lang/MainClass.java
@@ -21,5 +21,28 @@ public class MainClass {
 		
 		long timestamp = System.currentTimeMillis();
 		System.out.println(timestamp);
+		
+		System.out.println("=== identityHashCode() ===");
+		String h1 = "수박";
+		String h2 = "수박";
+		String h3 = new String("수박");
+		
+		System.out.println("** hashCode() **");
+		System.out.println("h1 : "+h1.hashCode());
+		System.out.println("h2 : "+h2.hashCode());
+		System.out.println("h3 : "+h3.hashCode());
+		
+		System.out.println("** identityHashCode() **");
+		System.out.println("h1 : "+System.identityHashCode(h1));
+		System.out.println("h2 : "+System.identityHashCode(h2));
+		System.out.println("h3 : "+System.identityHashCode(h3));
+		
+		System.out.println("=== 불변 객체 ===");
+		String str = "Hello";
+		System.out.println("초기 주소값 : "+System.identityHashCode(str));
+		str += ",World";
+		System.out.println("변경후주소값 : "+System.identityHashCode(str));
+		
+		
 	}
 }
