8709275b83a899c184543ab28ce2b96891c98c6c
devkgn <devkgn13@gmail.com>
2024-05-16 10:10:04 +0900

    [김가남] 파일 생성, 삭제

--- lec14_io/src/pkg01_file/FileClass.java
+++ lec14_io/src/pkg01_file/FileClass.java
@@ -1,18 +1,28 @@
 package pkg01_file;
 
 import java.io.File;
+import java.io.IOException;
 
 public class FileClass {
-	public static void main(String[] args) {
+	public static void main (String[] args) {
 		// 1. 디렉토리 작업
 		File dir = new File("\\storage\\test1");
 		if(dir.exists() == false) {
 			dir.mkdirs();
 		}
 		// 2. 파일 객체 추가
-		File file = new File(dir, "sample.txt");
+		File file = new File(dir,"sample.txt");
 		if(file.exists() == false) {
-			
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else {
+			file.delete();
 		}
+		
+		// 3. 파일 삭제
+		
 	}
 }
