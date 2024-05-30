80b7695b7bcd764a69cb97dced33eb853e21597e
devkgn <devkgn13@gmail.com>
2024-05-30 12:27:22 +0900

    [김가남] Transaction 작업

--- lec01_jdbc_basic/src/com/gn/run/E_Insert_Transaction.java
+++ lec01_jdbc_basic/src/com/gn/run/E_Insert_Transaction.java
@@ -3,6 +3,7 @@ package com.gn.run;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class E_Insert_Transaction {
@@ -37,12 +38,41 @@ public class E_Insert_Transaction {
 			if(rs.next()) {
 				result = rs.getInt("cnt");
 			}
-			
-			
-			
-			
+			// 6-2. INSERT 
+			if(result == 0) {
+				// 추가 가능(기존에 테스트7이 없으므로)
+				String sql2 = "INSERT INTO study(s_name) VALUES('"+sName+"')";
+				result = stmt.executeUpdate(sql2);
+				// INSERT 결과 출력
+				if(result > 0) {
+					System.out.println("성공적으로 추가되었습니다.");
+					
+				} else {
+					System.out.println("추가중 오류가 발생했습니다.");
+				}
+				
+			} else {
+				System.out.println("중복되는 이름이 있습니다.");
+			}
+			// 7. 성공적으로 작업이 끝났으면 commit
+			conn.commit();
 		}catch(Exception e) {
+			// 8. 실행 중 오류가 발생하면 rollback
+			try {
+				conn.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
 			e.printStackTrace();
+		}finally {
+			// 9. 종료
+			try {
+				rs.close();
+				stmt.close();
+				conn.close();
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
