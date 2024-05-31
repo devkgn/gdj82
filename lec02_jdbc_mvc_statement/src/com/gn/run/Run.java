c2d4aca95e4dd95d68d5df46723d7a83c48f425f
devkgn <devkgn13@gmail.com>
2024-05-31 09:51:32 +0900

    [김가남]메인메뉴 구성

--- lec02_jdbc_mvc_statement/src/com/gn/run/Run.java
+++ lec02_jdbc_mvc_statement/src/com/gn/run/Run.java
@@ -1,13 +1,16 @@
 package com.gn.run;
 
-import java.time.LocalDateTime;
-
-import com.gn.model.vo.Member;
+import com.gn.view.MemberMenu;
 
 public class Run {
 	public static void main(String[] args) {
-		Member m = new Member(1,"admin","1234","김철수","kcs@naver.com","0101112222",
-				"M",LocalDateTime.now(),LocalDateTime.now());
-		System.out.println(m);
+//		Member m = new Member(1,"admin","1234","김철수","kcs@naver.com","0101112222",
+//				"M",LocalDateTime.now(),LocalDateTime.now());
+//		System.out.println(m);
+		
+		// View -> MemeberMenu 클래스 가 가지고 있는 mainMenu() 메소드 호출
+		new MemberMenu().mainMenu();
+//		MemberMenu view = new MemberMenu();
+//		view.mainMenu();
 	}
 }
--- lec02_jdbc_mvc_statement/src/com/gn/view/MemberMenu.java
+++ lec02_jdbc_mvc_statement/src/com/gn/view/MemberMenu.java
@@ -1,5 +1,35 @@
 package com.gn.view;
 
-public class MemberMenu {
+import java.util.Scanner;
 
+public class MemberMenu {
+	
+	private Scanner sc = new Scanner(System.in);
+	
+	// 애플리케이션을 실행했을 때 사용자가 보게될 첫화면
+	public void mainMenu() {	
+		while(true) {
+			System.out.println("===== 회원 관리 =====");
+			System.out.println("1. 회원 추가");
+			System.out.println("2. 회원 전체 조회");
+			System.out.println("3. 회원 아이디 검색");		// 정확히 일치하는 경우
+			System.out.println("4. 회원 이름 키워드 검색");	// 검색어 포함된 경우
+			System.out.println("5. 회원 정보 수정");
+			System.out.println("6. 회원 탈퇴");
+			System.out.println("0. 프로그램 종료");
+			
+			System.out.println("메뉴 : ");
+			int menu = sc.nextInt();
+			sc.nextLine();
+			
+			switch(menu) {
+				case 1 : createMember(); break;
+				
+				case 0 : System.out.println("이용해주셔서 감사합니다.");return;
+				default : System.out.println("다시 입력해주세요.");break;
+			}
+			
+		}
+	}
+	
 }
